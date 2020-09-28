e0b6a4730e355578fd5810dc6829f5533578315f
koksharov1998 <koksharov1998@ya.ru>
2020-09-28 13:10:05 +0500

    change predict.sh, add some test data

--- predict.sh
+++ predict.sh
@@ -2,6 +2,16 @@
 python predict.py --crop_height=384 \
                   --crop_width=1248 \
                   --max_disp=192 \
+                  --data_path='/test/' \
+                  --test_list='lists/short_sceneflow_test.list' \
+                  --save_path='./result/' \
+                  --resume='./checkpoint/sceneflow_epoch_6.pth'
+				  
+exit
+
+python predict.py --crop_height=384 \
+                  --crop_width=1248 \
+                  --max_disp=192 \
                   --data_path='/ssd1/zhangfeihu/data/kitti/2015//testing/' \
                   --test_list='lists/kitti2015_test.list' \
                   --save_path='./result/' \
